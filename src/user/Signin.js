6ba17dd9c098289668fed1ccef2a496db425cacb
Julfikar Haidar <julfikar6262@gmail.com>
2020-12-18 12:09:24 +0600

    layout and route add

---
+++ src/user/Signin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../core/Layout';
+
+
+
+const Signin = () => {
+    return ( 
+        <Layout title="Signin" description="Node React E-commerce App">
+        <h1> This is signin page content  </h1> 
+    </Layout>
+     );
+}
+ 
+export default Signin;
---
+++ src/user/Signup.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import Layout from '../core/Layout';
+
+
+const Signup = () => {
+
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: "",
+        success: false
+    })
+
+
+    const handleChange = name => event => {
+        setValues({ ...values, error: false, [name]: event.target.value})
+    }
+
+    const signUpForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input onChange={handleChange('name')} type="text" className="form-control" />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input onChange={handleChange('email')} type="email" className="form-control" />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input onChange={handleChange('password')} type="password" className="form-control" />
+            </div>
+            <button className="btn btn-primary" type="submit">Submit</button>
+        </form>
+    )
+
+    return (
+        <Layout title="Signup" description="Node React E-commerce App" className="container col-md-8 offset-md-2">
+            {signUpForm()}
+            {JSON.stringify(values)}
+        </Layout>
+    );
+}
+
+export default Signup;
